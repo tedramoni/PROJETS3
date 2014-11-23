02da0974656702ff5c11e8a8e8c0f5e408da565f
Speleo99 <eagleframe@gmail.com>
2014-11-23 01:04:56 +0100

    Ajout ajout_client.js
    Javascript pour les formulaires dynamiques.

---
+++ Ikonic/js/ajout_client.js
@@ -0,0 +1,49 @@
+function addRowLivraison(tableID) {
+	var table = document.getElementById(tableID);
+	var rowCount = table.rows.length;
+	if(rowCount < 20){							// limit the user from creating fields more than your limits
+		var row = table.insertRow(rowCount);
+		var colCount = table.rows[0].cells.length;
+		for(var i=0; i<colCount; i++) {
+			var newcell = row.insertCell(i);
+			newcell.innerHTML = table.rows[0].cells[i].innerHTML;
+		}
+	}else{
+		 alert("Vous ne pouvez avoir plus de 20 adresses de livraison");
+			   
+	}
+}
+
+function addRowFacturation(tableID) {
+	var table = document.getElementById(tableID);
+	var rowCount = table.rows.length;
+	if(rowCount < 5){							// limit the user from creating fields more than your limits
+		var row = table.insertRow(rowCount);
+		var colCount = table.rows[0].cells.length;
+		for(var i=0; i<colCount; i++) {
+			var newcell = row.insertCell(i);
+			newcell.innerHTML = table.rows[0].cells[i].innerHTML;
+		}
+	}else{
+		 alert("Vous ne pouvez avoir plus de 5 adresses de facturation");
+			   
+	}
+}
+
+function deleteRow(tableID) {
+	var table = document.getElementById(tableID);
+	var rowCount = table.rows.length;
+	for(var i=0; i<rowCount; i++) {
+		var row = table.rows[i];
+		var chkbox = row.cells[0].childNodes[0];
+		if(null != chkbox && true == chkbox.checked) {
+			if(rowCount <= 1) { 						// limit the user from removing all the fields
+				alert("Vous devez entrer au moins une adresse.");
+				break;
+			}
+			table.deleteRow(i);
+			rowCount--;
+			i--;
+		}
+	}
+}
