34b53db259fe741d163fec2de0ffbefd77356e1d
MalikZouhiri <zouhiri.malik@gmail.com>
2014-11-30 21:36:40 +0100

    update process.php
    Correction du dernier commit : correction des boucles for.

--- process.php
+++ process.php
@@ -81,7 +81,7 @@
 				}
 				
 					//Insertion contacts (table contact)
-				for( $i= 0 ; $i < sizeof($code_contact); $i++ )
+				for($i=0;$i<sizeof($code_contact);$i++)
 				{
 					$req4 = "INSERT INTO contact ";
 					$req4.= " values('$code_contact[$i]', '$nom_contact[$i]', '$civilite[$i]', '$fonction[$i]', '$tel_bur[$i]', '$tel_mob[$i]',
