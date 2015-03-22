51f2d2517b0432e70d3451a503f0a2e725de9bec
Konderiano <azza_nordine@hotmail.fr>
2015-03-22 16:43:10 +0100

    correction erreur

--- Ikonic3/factures.sql
+++ Ikonic3/factures.sql
@@ -66,8 +66,8 @@ CREATE TABLE IF NOT EXISTS `factures` (
   `prix_ttc` float NOT NULL,
   `prix_ht` float NOT NULL,
   `raison_sociale` varchar(100) NOT NULL,
-  PRIMARY KEY (`num_bl`),
-  UNIQUE KEY `num_bl` (`num_bl`)
+  PRIMARY KEY (`num_facture`),
+  UNIQUE KEY `num_bl` (`num_facture`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=150001 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
